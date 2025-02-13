aea0e5b3941422392c1bb5fca6525601178ec2d3
Miguel Angel Saravia <msaravia@ia.com.mx>
2025-02-12 23:49:43 -0400

    - added project with swiftUI to github

---
+++ recepies/recepiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recepiesApp.swift
+//  recepies
+//
+//  Created by Miguel Angel Saravia Belmonte on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct recepiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recepiesTests/recepiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  recepiesTests.swift
+//  recepiesTests
+//
+//  Created by Miguel Angel Saravia Belmonte on 2/12/25.
+//
+
+import Testing
+@testable import recepies
+
+struct recepiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
